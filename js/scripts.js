7398184b0c08f5d64a47619638806f461d5ecccc
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-18 14:15:48 -0700

    Refactor code and add README.md template

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
-
-let suits = ["spades", "clubs", "hearts", "diamonds"];
-let cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
-
-suits.forEach(function(suit) {
-  cards.forEach(function(card) {
-    $("#result").append("<li>"+card+" of "+suit+"</li>");
+  let suits = ["spades", "clubs", "hearts", "diamonds"];
+  let cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
+  suits.forEach(function(suit) {
+    cards.forEach(function(card) {
+      $("#result").append("<li>"+card+" of "+suit+"</li>");
+    });
   });
-});
 
+  // Practice with Filter Method
+  const result = suits.filter(suit => suit.length > 5);
+  // console.log(result);
+
+  // Practice with Reduce Method
+  var numbers = [53, 88, 39];
+  var sum = numbers.reduce(function(total, amount) {
+    // console.log(total);
+    return total+amount;
+  })
+  // console.log(sum);
 });
